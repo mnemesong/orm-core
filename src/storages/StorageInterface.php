881d339292f0ad2b2dd40a8e5014f4963f318726
Pantagruel74 <tostar74@mail.ru>
2022-08-18 20:55:26 +0500

    Create StorageInterface.php

--- src/storages/StorageInterface.php
+++ src/storages/StorageInterface.php
@@ -2,18 +2,45 @@
 
 namespace Mnemesong\OrmCore\storages;
 
+use Mnemesong\OrmCore\command\DeleteCommand;
+use Mnemesong\OrmCore\command\SaveCommand;
+use Mnemesong\OrmCore\command\UpdateCommand;
 use Mnemesong\OrmCore\query\RecordsQuery;
 use Mnemesong\OrmCore\query\ScalarsQuery;
+use Mnemesong\Structure\Structure;
 
 interface StorageInterface
 {
     /**
      * @return RecordsQuery
      */
-    function selectRecords(): RecordsQuery;
+    function getSelectRecordsQuery(): RecordsQuery;
 
     /**
      * @return ScalarsQuery
      */
-    function selectScalars(): ScalarsQuery;
+    function getSelectScalarsQuery(): ScalarsQuery;
+
+    /**
+     * @param Structure $struct
+     * @return mixed
+     */
+    function getUpdateAllCommand(Structure $struct): UpdateCommand;
+
+    /**
+     * @return mixed
+     */
+    function getSaveOneSmartCommand(Structure $struct): SaveCommand;
+
+    /**
+     * @param Structure $struct
+     * @return SaveCommand
+     */
+    function getInsertOneHardCommand(Structure $struct): SaveCommand;
+
+    /**
+     * @return DeleteCommand
+     */
+    function getDeleteAllCommand(): DeleteCommand;
+
 }
