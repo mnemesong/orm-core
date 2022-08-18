aa600e7eb31bd3e616da007805402fe18c4c617c
Pantagruel74 <tostar74@mail.ru>
2022-08-18 18:34:34 +0500

    Add phpdoc to RecordsSaveModelInterface.php

--- src/storages/RecordsSaveModelInterface.php
+++ src/storages/RecordsSaveModelInterface.php
@@ -6,5 +6,10 @@ use Mnemesong\Structure\Structure;
 
 interface RecordsSaveModelInterface
 {
+    /**
+     * @param Structure $structure
+     * @param bool $smartSave
+     * @return void
+     */
     public function createRecord(Structure $structure, bool $smartSave): void;
 }
